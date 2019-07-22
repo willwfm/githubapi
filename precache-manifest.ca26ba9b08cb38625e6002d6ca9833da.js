c85fe65723f145f51f788ccc69d060a31fac3988
Willian Melo <willian.melo@somoseducacao.com.br>
2019-07-21 22:52:56 -0300

    Updates

---
+++ precache-manifest.ca26ba9b08cb38625e6002d6ca9833da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7419bd7c373ac8d241df8c5d895cc934",
+    "url": "/githubapi/index.html"
+  },
+  {
+    "revision": "8a262cb436c8f84dc39d",
+    "url": "/githubapi/static/css/main.f08642ec.chunk.css"
+  },
+  {
+    "revision": "09657c98c25502b15105",
+    "url": "/githubapi/static/js/2.5a362ca8.chunk.js"
+  },
+  {
+    "revision": "8a262cb436c8f84dc39d",
+    "url": "/githubapi/static/js/main.2876ea02.chunk.js"
+  },
+  {
+    "revision": "a1af48bf7c400da07849",
+    "url": "/githubapi/static/js/runtime~main.53d7a75b.js"
+  }
+]);
